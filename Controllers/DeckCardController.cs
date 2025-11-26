084d208af4257d938a1a0f32266b68c0d195057e
MattIRL <mlewis.tc@gmail.com>
2025-11-26 13:31:42 -0500

    Scaffolding and Views added

---
+++ Controllers/DeckCardController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LorcanaCardCollector.Models;
+
+namespace LorcanaCardCollector.Controllers
+{
+    public class DeckCardsController : Controller
+    {
+        private readonly CardsContext _context;
+
+        public DeckCardsController(CardsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DeckCards
+        public async Task<IActionResult> Index()
+        {
+            var deckCards = await _context.DeckCards
+                .Include(dc => dc.Card)
+                .Include(dc => dc.Deck)
+                .ToListAsync();
+            return View(deckCards);
+        }
+
+        // GET: DeckCards/Details?deckId=1&cardId=ARI-003
+        public async Task<IActionResult> Details(int deckId, string cardId)
+        {
+            var deckCard = await _context.DeckCards
+                .Include(dc => dc.Card)
+                .Include(dc => dc.Deck)
+                .FirstOrDefaultAsync(dc => dc.DeckId == deckId && dc.CardId == cardId);
+
+            if (deckCard == null)
+                return NotFound();
+
+            return View(deckCard);
+        }
+
+        // GET: DeckCards/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Decks = await _context.Decks.ToListAsync();
+            ViewBag.Cards = await _context.Cards.ToListAsync();
+            return View();
+        }
+
+        // POST: DeckCards/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DeckId,CardId,QuantityInDeck")] DeckCard deckCard)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if the combination already exists
+                var existing = await _context.DeckCards
+                    .FirstOrDefaultAsync(dc => dc.DeckId == deckCard.DeckId && dc.CardId == deckCard.CardId);
+                if (existing != null)
+                {
+                    ModelState.AddModelError("", "This card is already in the deck.");
+                    ViewBag.Decks = await _context.Decks.ToListAsync();
+                    ViewBag.Cards = await _context.Cards.ToListAsync();
+                    return View(deckCard);
+                }
+
+                _context.Add(deckCard);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Decks = await _context.Decks.ToListAsync();
+            ViewBag.Cards = await _context.Cards.ToListAsync();
+            return View(deckCard);
+        }
+
+        // GET: DeckCards/Edit?deckId=1&cardId=ARI-003
+        public async Task<IActionResult> Edit(int deckId, string cardId)
+        {
+            var deckCard = await _context.DeckCards
+                .FirstOrDefaultAsync(dc => dc.DeckId == deckId && dc.CardId == cardId);
+
+            if (deckCard == null)
+                return NotFound();
+
+            ViewBag.Decks = await _context.Decks.ToListAsync();
+            ViewBag.Cards = await _context.Cards.ToListAsync();
+            return View(deckCard);
+        }
+
+        // POST: DeckCards/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("DeckId,CardId,QuantityInDeck")] DeckCard deckCard)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(deckCard);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Decks = await _context.Decks.ToListAsync();
+            ViewBag.Cards = await _context.Cards.ToListAsync();
+            return View(deckCard);
+        }
+
+        // GET: DeckCards/Delete?deckId=1&cardId=ARI-003
+        public async Task<IActionResult> Delete(int deckId, string cardId)
+        {
+            var deckCard = await _context.DeckCards
+                .Include(dc => dc.Card)
+                .Include(dc => dc.Deck)
+                .FirstOrDefaultAsync(dc => dc.DeckId == deckId && dc.CardId == cardId);
+
+            if (deckCard == null)
+                return NotFound();
+
+            return View(deckCard);
+        }
+
+        // POST: DeckCards/DeleteConfirmed
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int deckId, string cardId)
+        {
+            var deckCard = await _context.DeckCards
+                .FirstOrDefaultAsync(dc => dc.DeckId == deckId && dc.CardId == cardId);
+
+            if (deckCard != null)
+            {
+                _context.DeckCards.Remove(deckCard);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Controllers/DeckController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LorcanaCardCollector.Models;
+
+namespace LorcanaCardCollector.Controllers
+{
+    public class DeckController : Controller
+    {
+        private readonly CardsContext _context;
+
+        public DeckController(CardsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Deck
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Decks.ToListAsync());
+        }
+
+        // GET: Deck/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deck = await _context.Decks
+                .FirstOrDefaultAsync(m => m.DeckId == id);
+            if (deck == null)
+            {
+                return NotFound();
+            }
+
+            return View(deck);
+        }
+
+        // GET: Deck/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deck/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DeckId,DeckName,DeckDescription,AccessKey")] Deck deck)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(deck);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(deck);
+        }
+
+        // GET: Deck/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deck = await _context.Decks.FindAsync(id);
+            if (deck == null)
+            {
+                return NotFound();
+            }
+            return View(deck);
+        }
+
+        // POST: Deck/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DeckId,DeckName,DeckDescription,AccessKey")] Deck deck)
+        {
+            if (id != deck.DeckId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(deck);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DeckExists(deck.DeckId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(deck);
+        }
+
+        // GET: Deck/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var deck = await _context.Decks
+                .FirstOrDefaultAsync(m => m.DeckId == id);
+            if (deck == null)
+            {
+                return NotFound();
+            }
+
+            return View(deck);
+        }
+
+        // POST: Deck/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var deck = await _context.Decks.FindAsync(id);
+            if (deck != null)
+            {
+                _context.Decks.Remove(deck);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DeckExists(int id)
+        {
+            return _context.Decks.Any(e => e.DeckId == id);
+        }
+    }
+}
